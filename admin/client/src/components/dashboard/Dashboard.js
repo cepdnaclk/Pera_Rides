7f4bc12930e11610fa22e18449ec4adef63647f6
Sasindu Dilshan <sasindudil0002@gmail.com>
2024-01-09 00:38:42 +0530

    progressbar and statBox added

--- admin/client/src/components/dashboard/Dashboard.js
+++ admin/client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,26 @@
 import styled from "styled-components";
 import HeaderTitle from "../headerTitle/HeaderTitle";
+// import { Box, IconButton, Typography, useTheme } from "@mui/material";
+// import { tokens } from "../../theme";
+// import { mockTransactions } from "../../data/mockData";
+// import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+// import EmailIcon from "@mui/icons-material/Email";
+// import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+// import PersonAddIcon from "@mui/icons-material/PersonAdd";
+// import TrafficIcon from "@mui/icons-material/Traffic";
+// import LineChart from "../linechart/LineChart";
+// import BarChart from "../barchart/Barchart";
+// import StatBox from "../StatBox/StatBox";
+// import ProgressCircle from "../progressCircle/ProgressCircle";
 
 const DashboardMain = styled.div`
   width: 100%;
   height: 100%;
-  /* background-color: red; */
 `;
 
 const Dashboard = () => {
+  // const theme = useTheme();
+  // const colors = tokens(theme.palette.mode);
   return (
     <DashboardMain>
       <HeaderTitle title={"Dashboard"} desc="welcome to your dashboard" />
